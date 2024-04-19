09316d48c4a291ea1e5a6b81921a362a645a0136
MrNwear <mr.y.neuer@gmail.com>
2024-04-20 00:08:45 +0200

    Initial Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by yousef neuer on 20/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
